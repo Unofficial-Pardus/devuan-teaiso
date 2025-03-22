0222bd8e8963f031ed3948d4a98195a19c957703
mobilturka <162546868+03tekno@users.noreply.github.com>
2025-03-22 05:58:26 +0300

    Update customize-airootfs.sh

--- devuan/customize-airootfs.sh
+++ devuan/customize-airootfs.sh
@@ -19,6 +19,8 @@ wget https://github.com/03tekno/debhane/raw/main/pardus-package-installer_0.6.2_
 wget https://github.com/03tekno/debhane/raw/main/pipewire-launcher_1.0.0_all.deb
 yes | apt install ./*.deb -yq --allow-downgrades
 
+yes | apt remove pulseaudio
+
 install -d -m 0755 /etc/apt/keyrings
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
